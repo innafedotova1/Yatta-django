684d7756b8eca528193743641ad3cbefe9f04efe
theashe <vkrivulyak@bk.ru>
2025-06-09 02:26:59 +0500

    еремещена статика: добавлен style.css, удалены дубликаты

--- souka/templates/souka/src/js/lesson.js
+++
@@ -1,6 +0,0 @@
-// Для динамически добавленных элементов
-document.body.addEventListener('click', function(e) {
-  if (e.target.closest('.notes-toggle')) {
-    e.target.closest('.lesson__notes').classList.toggle('active');
-  }
-});
