13fad187e623edfaadd50860fe190271664d6958
theashe <vkrivulyak@bk.ru>
2025-06-09 03:28:20 +0500

    ересобрана статика и обновлён settings.py

--- sitesouka/settings.py
+++ sitesouka/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o0tx^nw-^)+qnz+-ugxf9vlq7bve+ksl-p#manod17twjymka)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-81b6.up.railway.app']
